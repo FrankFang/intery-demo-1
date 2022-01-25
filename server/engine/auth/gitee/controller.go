9a2583aaea9645f351dcb1866b46dcfb355d6e9b
frankfang <frankfang1990@gmail.com>
2022-01-26 04:58:11 +0800

    添加 gitee 登录

--- server/engine/auth/gitee/controller.go
+++ server/engine/auth/gitee/controller.go
@@ -1,57 +1,129 @@
 package gitee
 
 import (
+	"encoding/json"
 	"fmt"
+	"intery/server/config"
+	"intery/server/database"
+	"intery/server/model"
 	"io/ioutil"
+	"net/http"
+	"time"
 
 	"github.com/dchest/uniuri"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/oauth2"
 )
 
 type Controller struct{}
-
-var conf = &oauth2.Config{
-	ClientID:     "ee73eb425c78c965e4b749ec7fb250beee30449fac584ac8b13e0e2a92dd74fe",
-	ClientSecret: "61484b0b39a3d974dbd031b149e152fd469f27368ae600ec804eb438d4c4fafe",
-	Scopes:       []string{"projects", "emails"},
-	RedirectURL:  "http://intery.xiedaimala.com/api/v1/auth/gitee_callback",
-	Endpoint: oauth2.Endpoint{
-		AuthURL:  "https://gitee.com/oauth/authorize",
-		TokenURL: "https://gitee.com/oauth/token",
-	},
+type GiteeUser struct {
+	Login     string `json:"login"`
+	Id        int64  `json:"id"`
+	AvatarUrl string `json:"avatar_url"`
+	Name      string `json:"name"`
 }
 
+var conf = Conf
+
 func (ctrl Controller) Show(c *gin.Context) {
-	url := conf.AuthCodeURL(uniuri.New())
+	state := uniuri.New()
+	config.AddOAuth2State(state)
+	url := conf.AuthCodeURL(state)
 	c.JSON(200, gin.H{
 		"url": url,
 	})
 }
+
 func (ctrl Controller) Callback(c *gin.Context) {
-	code, hasCode := c.GetQuery("code")
-	if !hasCode {
-		c.JSON(400, gin.H{
-			"reason": "no code",
+	var params struct {
+		Code  string `json:"code"`
+		State string `json:"state"`
+	}
+	if err := c.BindJSON(&params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"reason": err.Error()})
+		return
+	}
+	if !config.UseOAuth2State(params.State) {
+		c.JSON(http.StatusBadRequest, gin.H{"reason": "state 错误"})
+		return
+	}
+
+	token, err := conf.Exchange(c, params.Code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"reason": "exchange token via code failed",
 		})
 		return
 	}
-	tok, err := conf.Exchange(c, code)
-	if err != nil {
-		fmt.Println(err)
+	client := conf.Client(c, token)
+	defer client.CloseIdleConnections()
+	for i := 0; i < 2; i++ {
+		response, err := client.Get("https://gitee.com/api/v5/user")
+		if err != nil {
+			continue
+		}
+		bytes, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			continue
+		}
+		giteeUser := GiteeUser{}
+		err = json.Unmarshal(bytes, &giteeUser)
+		if err != nil {
+			continue
+		}
+		u := database.GetQuery().User
+		var user *model.User
+		a := database.GetQuery().Authorization
+		auth, err := a.WithContext(c).Where(a.Login.Eq(giteeUser.Login)).Where(a.Provider.Eq("gitee")).FirstOrInit()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"reason": "Authorization not found. " + err.Error(),
+			})
+			return
+		}
+		if auth.UserId == 0 {
+			name := giteeUser.Name
+			if name == "" {
+				name = giteeUser.Login
+			}
+			user = &model.User{Name: name}
+			if err := u.WithContext(c).Create(user); err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"reason": "create user failed. " + err.Error(),
+				})
+				return
+			}
+			auth.UserId = user.ID
+		} else {
+			user, err = u.WithContext(c).Where(u.ID.Eq(auth.UserId)).First()
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{
+					"reason": "find user failed. " + err.Error(),
+				})
+				return
+			}
+		}
+		auth.AccessToken = token.AccessToken
+		auth.TokenType = token.TokenType
+		auth.RefreshToken = token.RefreshToken
+		auth.Expiry = token.Expiry
+		auth.TokenGeneratedAt = time.Now()
+		auth.AvatarUrl = giteeUser.AvatarUrl
+		auth.Name = giteeUser.Name
+		auth.VendorId = fmt.Sprintf("%v", giteeUser.Id)
+		if err = database.GetQuery().WithContext(c).Authorization.Save(auth); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+		}
+		if t, err := user.JWT(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"jwt": t})
+		}
+		break
 	}
-
-	client := conf.Client(c, tok)
-	response, err := client.Get("https://gitee.com/api/v5/user")
-	if err != nil {
-		fmt.Println(err)
+	if !c.Writer.Written() {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"reason": "无法获取 Gitee User 信息",
+		})
 	}
-	bytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	w := c.Writer
-	w.Header().Set("Content-Type", "application/json;charset=utf-8")
-	_, _ = w.Write(bytes)
 }
--- server/engine/router.go
+++ server/engine/router.go
@@ -2,7 +2,6 @@ package engine
 
 import (
 	"fmt"
-	"intery/server/engine/auth/gitea"
 	"intery/server/engine/auth/gitee"
 	"intery/server/engine/auth/github"
 	"intery/server/engine/deploy"
@@ -55,10 +54,10 @@ func NewRouter() *gin.Engine {
 				authGroup.POST("/github_callback", g.Callback)
 				t := gitee.Controller{}
 				authGroup.GET("/gitee", t.Show)
-				authGroup.GET("/gitee_callback", t.Callback)
-				a := gitea.Controller{}
-				authGroup.GET("/gitea", a.Show)
-				authGroup.GET("/gitea_callback", a.Callback)
+				authGroup.POST("/gitee_callback", t.Callback)
+				// a := gitea.Controller{}
+				// authGroup.GET("/gitea", a.Show)
+				// authGroup.POST("/gitea_callback", a.Callback)
 			}
 			logGroup := v1.Group("logs")
 			{
